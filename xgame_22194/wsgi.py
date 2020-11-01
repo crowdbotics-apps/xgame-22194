33047569b4c892ffd2bed5eebde7371a1cc8fefc
Crowdbotics <team@crowdbotics.com>
2020-11-01 22:02:00 +0000

    Update models

--- xgame_22194/wsgi.py
+++ xgame_22194/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xgame_22194.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xgame_22194.settings")
 
 application = get_wsgi_application()
